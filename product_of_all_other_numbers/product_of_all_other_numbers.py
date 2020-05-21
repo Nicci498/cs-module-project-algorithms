30e585467ff4448946f63335ae42070955e84598
Nicole white <you@example.com>
2020-05-20 15:08:13 -1000

    products producting

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,17 +2,36 @@
 Input: a List of integers
 Returns: a List of integers
 
-write a function that takes one index 
-and multiplies it by all the other indexes excluding itself
-use that function in list comp
-return [x.function() for x in arr2]
+find total products of array to left
+find total '' to right
+multiply left and right and append value to new arr
 
 '''
 
-def product_of_all_other_numbers(arr2):
-    # Your code here
+def product_of_all_other_numbers(arr):
+    # empty bucket for new arr
+    product_arr = []
+    # loop through list
+    for i in range(len(arr)): 
+        #get values to the left of given index
+        left = arr[:i]
+        # and to right
+        right = arr[i + 1:]
+        # combine left and right
+        combined = left + right
+        #initiate a bucket for products (since multiplying must be 1 not zero)
+        total = 1
+        #for each element in the left-right total
+        for e in combined:
+            #add to bucket
+            total = total * e
+            #append to new arr
+        product_arr.append(total)
 
-    pass
+    return product_arr
+
+        #val = total products left * total products right
+        #append total to value    
 
 
 if __name__ == '__main__':
@@ -21,4 +40,5 @@ if __name__ == '__main__':
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
-    print(prod(arr2))
+    # print(arr2[1:])
+    # print(arr2[:4])exclusive
